4a7e55aa24276b267efb20fe27b5f50a480a0601
Martin Harding <martin@martinharding.com>
2017-12-01 21:03:23 -0500

    Add pathfinding to setup.py requirements

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(name='ktaned',
       description='Solve Keep Talking and Nobody Explodes bombs',
-   	  license='MIT',
-   	  author='Martin Harding',
+      license='MIT',
+      author='Martin Harding',
       author_email='martin@martinharding.com',
       packages=['ktaned'],
-      install_requires=[],
+      install_requires=[
+        'pathfinding'
+      ],
       zip_safe=False)
