7c40bcd995bb5bdd5a354f4ceaf1a89af40c593a
Marin Harding <martin@martinharding.com>
2017-12-02 17:38:29 -0500

    Fix pep8 formatting issues in simple_wires.py

--- ktaned/simple_wires.py
+++ ktaned/simple_wires.py
@@ -9,7 +9,7 @@ class SimpleWires(object):
     def add_wire(self, color):
         if color not in self.valid_colors:
             raise Exception('Color ({}) must be one of {}'
-                .format(color, self.valid_colors))
+                            .format(color, self.valid_colors))
         self.wires.append(color)
 
     def set_wires(self, wires):
@@ -27,47 +27,47 @@ class SimpleWires(object):
 
         if wire_count is 3:
             if 'red' not in self.wires:
-                return 2 # cut second
+                return 2  # cut second
             elif self.wires[-1] is 'white':
-                return wire_count # cut last
+                return wire_count  # cut last
             elif 'blue' in self.wires:
-                return self.last_color('blue') # cut last blue
+                return self.last_color('blue')  # cut last blue
             else:
-                return wire_count # cut last
+                return wire_count  # cut last
 
         elif wire_count is 4:
             if 'red' in self.wires and int(bomb.serial[-1]) % 2:
-                return self.last_color('red') # cut last red
+                return self.last_color('red')  # cut last red
             elif ('red' not in self.wires and
                   self.wires[-1] is 'yellow' or
                   self.wires.count('blue') is 1):
-                return 1 # cut first
+                return 1  # cut first
             elif 'yellow' in self.wires:
-                return wire_count # cut last
+                return wire_count  # cut last
             else:
-                return 2 # cut second
+                return 2  # cut second
 
         elif wire_count is 5:
             if self.wires[-1] is 'black' and int(bomb.serial[-1]) % 2:
-                return 4 # cut fourth
+                return 4  # cut fourth
             elif (self.wires.count('red') is 1 and
                   self.wires.count('yellow') is 1):
-                return 1 # cut first
+                return 1  # cut first
             elif 'black' not in self.wires:
-                return 2 # cut second
+                return 2  # cut second
             else:
-                return 1 # cut first
+                return 1  # cut first
 
         elif wire_count is 6:
             if 'yellow' not in self.wires and int(bomb.serial[-1]) % 2:
-                return 3 # cut third
+                return 3  # cut third
             elif (self.wires.count('yellow') is 1 and
                   self.wires.count('white') is 1):
-                return 4 # cut fourth
+                return 4  # cut fourth
             elif 'red' not in self.wires:
-                return wire_count # cut last
+                return wire_count  # cut last
             else:
-                return 4 # cut fourth
+                return 4  # cut fourth
 
         else:
             raise Exception(
