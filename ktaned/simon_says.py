fdc0b079bd29da2df8732da8efc5f49a81a7e045
Julian Norton <Julian@JulianNorton.com>
2017-12-15 11:42:07 -0500

    updating simon says, removing verboose checks and strikes

--- ktaned/simon_says.py
+++ ktaned/simon_says.py
@@ -1,15 +1,13 @@
-class Simon_says(object):
+class SimonSays(object):
     """Represents the the simon says module"""
 
     def __init__(self, bomb):
-        super(Simon_says, self).__init__()
+        super(SimonSays, self).__init__()
         self.bomb = bomb
         self.valid_colors = ['red', 'blue', 'green', 'yellow']
-        # TODO, get strikes from bomb case?
-        self.valid_strikes = [0, 1 , 2]
-        # TODO, change vowel to get it from bomb case
-        # use bomb.check_serial_for_vowel() ?
-        self.vowel = False
+        # TODO, get strikes from bomb case, get_bomb_strikes() doesn't exist.
+        self.strikes = bomb.get_bomb_strikes()
+        self.vowel = bomb.check_serial_for_vowel()
 
     def set_color(self, color):
         """Checks if the input is a valid color"""
@@ -17,15 +15,8 @@ class Simon_says(object):
             raise Exception('Color ({}) must be one of {}'
                             .format(color, self.valid_colors))
         self.color = color
-    
-    def set_strike(self, strike):
-        """Checks if the strikes is a valid number"""
-        if strike not in self.valid_strikes:
-            raise Exception('Strike ({}) must be one of {}'
-                            .format(strike, self.valid_strikes))
-        self.strike = strike
 
-    def solve(self, vowel, strikes):
+    def get_push_color(self, color):
         """Returns the translated colors for simon says"""
         if not hasattr(self, 'color') and not hasattr(self, 'label'):
             raise Exception('Must set color and label before getting action')
@@ -43,7 +34,7 @@ class Simon_says(object):
             raise Exception('Invalid length for simon says:\n 1 < items < 5')
 
         # vowel and strikes determine the correct color translation
-        if vowel == False:
+        if not vowel:
             if strikes == 0:
                 for i in range(len(simon_list)):
                     print(simon_list[i])
@@ -78,7 +69,7 @@ class Simon_says(object):
                     elif simon_list[i] == 'yellow':
                         simon_translated.append('red')
 
-        elif vowel == True:
+        elif vowel:
             if strikes == 0:
                 for i in range(len(simon_list)):
                     print(simon_list[i])
@@ -112,8 +103,6 @@ class Simon_says(object):
                         simon_translated.append('yellow')
                     elif simon_list[i] == 'yellow':
                         simon_translated.append('blue')
-        else:
-            return 'Simon says solution failed'
 
         return simon_translated
         
