b7fd9d2ccd0294ee386fa6390644a19a64a96c29
Julian Norton <julian@juliannorton.com>
2017-12-17 15:09:33 -0500

    adding required pipy fields

--- setup.py
+++ setup.py
@@ -1,15 +1,29 @@
 from setuptools import setup
 
-setup(name='ktaned',
-      description='Solve Keep Talking and Nobody Explodes bombs',
-      license='MIT',
-      author='Martin Harding',
-      author_email='martin@martinharding.com',
-      packages=['ktaned'],
-      install_requires=[
+setup(
+    name='ktaned',
+    version='1.0a1',
+    description='Solver for Keep Talking and Nobody Explodes bombs',
+    url='https://github.com/MartinHarding/ktaned/',
+    author='Martin Harding',
+    license='MIT',
+    author_email='martin@martinharding.com',
+    keywords='KTANE, keep talking and nobody explodes'
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: KTANE Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+    ],
+    packages=['ktaned'],
+    install_requires=[
+        'random',
+        'inflect',
         'pathfinding',
         'PyYAML'
-      ],
-      zip_safe=True,
-      include_package_data=True,
-      package_data={'': '*.yml'})
+    ],
+    python_requires='>=3.6',
+    zip_safe=True,
+    include_package_data=True,
+    package_data={'': '*.yml'})
